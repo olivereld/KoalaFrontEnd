1263b0876d282d7c81f14c580d6be2fad63a0e75
olivereld <programadorctg@gmail.com>
2022-06-20 13:37:10 -0400

    Para el backend no administrable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccessGuard } from './access.guard';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   { 
     path: '',    
-    loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)    
+    pathMatch:'full',
+    redirectTo:'notfound/404'
+    //loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule)    
   },  
-  { 
-    path: 'dashboard',  
-    canActivate:[ AccessGuard],
-    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)    
-  },  
+  //{ 
+  //  path: 'dashboard',  
+  //  canActivate:[ AccessGuard],
+  //  loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)    
+  //},  
   {
     path:':id',
     loadChildren: () => import('./consulta/consulta.module').then(m => m.ConsultaModule)    
